eefb17c6a4bfb4c053794f73d1d2f5e3c606fbaa
ShinySaana <shinysaana@gmail.com>
2019-09-16 20:04:06 +0200

    base window

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,6 @@
+#[macro_use]
+extern crate glium;
+
 pub mod bridge;
 pub mod pty;
+pub mod window;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use termiku::bridge::spawn_process;
+use termiku::window::window;
 
 fn main() {
-    spawn_process("sh", &[]);
+    // spawn_process("sh",
+    window()
 }
