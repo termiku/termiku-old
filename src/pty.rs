0f0d34905e6b7eaab9d926abf588ce215403eaf9
LunarLambda <lunarlambda@gmail.com>
2019-11-06 18:59:56 +0100

    Fix up documentation for pty::Pty.

--- src/pty.rs
+++ src/pty.rs
@@ -6,22 +6,27 @@ use std::os::unix::{io::*, process::CommandExt};
 use std::process::{Child, Command, Stdio};
 use std::ptr;
 
-/// A pair of `RawFds` referring to the pseudoterminal multiplexer device (ptmx), and its associated pseudoterminal sub-device (pts).
+/// A pair of `RawFds` referring to the pseudoterminal multiplexer device (ptmx), and the associated pseudoterminal sub-device (pts).
 #[derive(Clone, Copy, Debug)]
 pub struct RawPtyFds {
     pub ptmx: RawFd,
     pub pts:  RawFd,
 }
 
-/// A Pseudoterminal.
+/// A pseudoterminal (sometimes abbreviated "pty") is a pair of virtual character devices that provide a bidirectional communication channel.
+///
+/// For more information, see, the Linux manual pages [pty(7)] and [pts(4)].
+/// 
+/// [pty(7)]: http://man7.org/linux/man-pages/man7/pty.7.html
+/// [pts(4)]: http://man7.org/linux/man-pages/man4/pts.4.html
+#[derive(Debug)]
 pub struct Pty {
     fds: RawPtyFds
 }
 
 impl Pty {
     /// Opens a new pseudoterminal.
-    /// The created file descriptors have the flags `O_RDWR`, `O_NOCTTY`.
-    /// The descriptor for the multiplexer device additionally has `O_NONBLOCK` set.
+    /// The descriptor for the ptmx device is additionally set to be nonblocking.
     pub fn open() -> io::Result<Pty> {
         let mut ptmx = -1;
         let mut pts  = -1;
@@ -57,7 +62,9 @@ impl Pty {
     /// Returns the `RawFd` for this pseudoterminal's ptmx device.
     /// This does not transfer ownership of the file descriptor to the caller.
     /// 
-    /// See also the `std::os::unix::io::AsRawFd` trait.
+    /// See also the [`std::os::unix::io::AsRawFd` trait.][AsRawFd]
+    /// 
+    /// [AsRawFd]: https://doc.rust-lang.org/std/os/unix/io/trait.AsRawFd.html
     pub fn raw_ptmx_fd(&self) -> RawFd {
         self.fds.ptmx
     }
@@ -65,7 +72,9 @@ impl Pty {
     /// Returns the `RawFd` for this pseudoterminal's pts device.
     /// This does not transfer ownership of the file descriptor to the caller.
     /// 
-    /// See also the `std::os::unix::io::AsRawFd` trait.
+    /// See also the [`std::os::unix::io::AsRawFd` trait.][AsRawFd]
+    /// 
+    /// [AsRawFd]: https://doc.rust-lang.org/std/os/unix/io/trait.AsRawFd.html
     pub fn raw_pts_fd(&self) -> RawFd {
         self.fds.pts
     }
@@ -73,7 +82,9 @@ impl Pty {
     /// Returns the `RawFd` pair for this pseudoterminal.
     /// This does not transfer ownership of the file descriptors to the caller.
     ///
-    /// See also the `std::os::unix::io::AsRawFd` trait.
+    /// See also the [`std::os::unix::io::AsRawFd` trait.][AsRawFd]
+    /// 
+    /// [AsRawFd]: https://doc.rust-lang.org/std/os/unix/io/trait.AsRawFd.html
     pub fn as_raw_fds(&self) -> RawPtyFds {
         self.fds
     }
@@ -81,7 +92,9 @@ impl Pty {
     /// Consumes this pseudoterminal, returning its `RawFd` pair.
     /// This transfers ownership of the file descriptors to the caller.
     /// 
-    /// See also the `std::os::unix::io::IntoRawFd` trait.
+    /// See also the [`std::os::unix::io::IntoRawFd` trait.][IntoRawFd]
+    /// 
+    /// [IntoRawFd]: https://doc.rust-lang.org/std/os/unix/io/trait.IntoRawFd.html
     pub fn into_raw_fds(self) -> RawPtyFds {
         let fds = self.fds;
         mem::forget(self);
@@ -90,6 +103,7 @@ impl Pty {
 }
 
 impl io::Read for Pty {
+    /// Reads data directly from the ptmx descriptor for this pseudoterminal.
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         let res = unsafe {
             libc::read(self.fds.ptmx, buf.as_mut_ptr() as _, buf.len())
@@ -104,6 +118,7 @@ impl io::Read for Pty {
 }
 
 impl io::Write for Pty {
+    /// Writes data directly to the ptmx descriptor for this pseudoterminal.
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         let res = unsafe {
             libc::write(self.fds.ptmx, buf.as_ptr() as _, buf.len())
@@ -116,18 +131,37 @@ impl io::Write for Pty {
         }
     }
 
-    /// Always returns Ok because we don't have any buffered data to flush.
+    /// This function is a no-op as the pseudoterminal never buffers any data.
     fn flush(&mut self) -> io::Result<()> {
         Ok(())
+    }
+}
+
+use std::fmt;
+
+impl fmt::Display for Pty {
+    /// Shows the path for this pseudoterminal's pts device.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use std::ffi::CStr;
+
+        unsafe {
+            let ptsname = libc::ptsname(self.fds.ptmx);
+            let cstr    = CStr::from_ptr(ptsname)
+                               .to_str()
+                               .unwrap();
+            f.write_str(cstr)
+        }
     }
 }
 
 impl Drop for Pty {
     /// Closes the pseudoterminal.
     /// 
-    /// We do not check for errors for the same reason Rust itself doesn't:
-    /// "[...] if an error occurs we don't actually know if he file descriptor was closed or not"
-    /// See https://github.com/rust-lang/rust/blob/6de4924b6c1ff5a99397ca1a3894c51f085f3e6f/src/libstd/sys/unix/fd.rs#L274.
+    /// No error checks are performed for the same reason Rust itself doesn't:
+    /// *"[...] if an error occurs we don't actually know if he file descriptor was closed or not"*
+    /// See [the implementation for file descriptors in Rust.][github]
+    /// 
+    /// [github]: https://github.com/rust-lang/rust/blob/6de4924b6c1ff5a99397ca1a3894c51f085f3e6f/src/libstd/sys/unix/fd.rs#L274.
     fn drop(&mut self) {
         unsafe {
             libc::close(self.fds.ptmx);
