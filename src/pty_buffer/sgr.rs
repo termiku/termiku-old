702d86f87b4f93f8787d84d8870be63b542c29a7
ShinySaana <shinysaana@gmail.com>
2019-12-09 22:15:51 +0100

    Add 16 colors support

---
+++ src/pty_buffer/sgr.rs
@@ -0,0 +1,102 @@
+use super::*;
+
+impl Screen {
+    pub fn reset_graphics(&mut self) {
+        self.cursor.properties = CellProperties::new();
+    }
+
+    // TODO: be able to configurable
+    fn get_simple_color(&mut self, color: u8) -> Color {
+        SimpleColor::from_u8(color).to_color()
+    }
+        
+    pub fn simple_color_foreground(&mut self, color: u8) {        
+        self.cursor.properties.fg = self.get_simple_color(color);
+    }
+    
+    pub fn simple_color_background(&mut self, color: u8) {
+        self.cursor.properties.bg = self.get_simple_color(color);
+    }
+    
+    pub fn default_color_foreground(&mut self) {
+        self.cursor.properties.fg = CellProperties::new().fg;
+    }
+    
+    pub fn default_color_background(&mut self) {
+        self.cursor.properties.bg = CellProperties::new().bg;
+    }
+
+}
+
+/// Simple terminal colors, as defined by xterm.
+/// https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+/// 4th parameter is alpha channel, which will always be 255 for simple colors
+
+#[repr(u8)]
+enum SimpleColor {
+    Black = 0,
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    Magenta,
+    Cyan,
+    White,
+    BrightBlack,
+    BrightRed,
+    BrightGreen,
+    BrightYellow,
+    BrightBlue,
+    BrightMagenta,
+    BrightCyan,
+    BrightWhite,
+}
+
+impl SimpleColor {
+    pub fn from_u8(byte: u8) -> Self {
+        use SimpleColor::*;
+        
+        match byte {
+            0 => Black,
+            1 => Red,
+            2 => Green,
+            3 => Yellow,
+            4 => Blue,
+            5 => Magenta,
+            6 => Cyan,
+            7 => White,
+            8 => BrightBlack,
+            9 => BrightRed,
+            10 => BrightGreen,
+            11 => BrightYellow,
+            12 => BrightBlue,
+            13 => BrightMagenta,
+            14 => BrightCyan,
+            15 => BrightWhite,
+            _ => unreachable!()
+        }
+    }
+    
+    pub fn to_color(&self) -> Color {
+        use SimpleColor::*;
+        
+        match self {
+            Black => Color(0, 0, 0, 255),
+            Red => Color(205, 0, 0, 255),
+            Green => Color(0, 205, 0, 255),
+            Yellow => Color(205, 205, 0, 255),
+            Blue => Color(0, 0, 238, 255),
+            Magenta => Color(205, 0, 205, 255),
+            Cyan => Color(0, 205, 205, 255),
+            White => Color(229, 229, 229, 255),
+            BrightBlack => Color(127, 127, 127, 255),
+            BrightRed => Color(255, 0, 0, 255),
+            BrightGreen => Color(0, 255, 0, 255),
+            BrightYellow => Color(255, 255, 0, 255),
+            BrightBlue => Color(0, 0, 252, 255),
+            BrightMagenta => Color(255, 0, 255, 255),
+            BrightCyan => Color(0, 255, 255, 255),
+            BrightWhite => Color(255, 255, 255, 255),
+        }
+    }
+}
